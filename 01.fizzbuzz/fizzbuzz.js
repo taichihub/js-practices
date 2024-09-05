bafe49e73f800fd310cf6d46dccc4737f85550fb
taichihub <taichikuniv21@gmail.com>
2024-09-05 15:28:03 +0900

    FizzBuz問題

---
+++ 01.fizzbuzz/fizzbuzz.js
@@ -0,0 +1,13 @@
+for (let i = 1; i <= 20; i++) {
+  let output = "";
+  if (i % 3 === 0) {
+    output += "Fizz";
+  }
+  if (i % 5 === 0) {
+    output += "Buzz";
+  }
+  if (output === "") {
+    output = i;
+  }
+  console.log(output);
+}
