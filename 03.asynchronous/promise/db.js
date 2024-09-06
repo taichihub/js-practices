18aca3f5bba51a1d26de57369532618203e41506
taichihub <taichikuniv21@gmail.com>
2024-09-06 17:50:23 +0900

    promise処理共通部分

---
+++ 03.asynchronous/promise/db.js
@@ -0,0 +1,32 @@
+import sqlite3 from "sqlite3";
+
+export const db = new sqlite3.Database(":memory:");
+
+export function run(sql, params = []) {
+  return new Promise((resolve, reject) => {
+    db.run(sql, params, function (err) {
+      if (err) {
+        reject(err);
+      } else {
+        resolve({ lastID: this.lastID, changes: this.changes });
+      }
+    });
+  });
+}
+
+export function all(sql, params = []) {
+  return new Promise((resolve, reject) => {
+    db.all(sql, params, (err, rows) => {
+      if (err) {
+        reject(err);
+      } else {
+        resolve(rows);
+      }
+    });
+  });
+}
+
+export function close() {
+  db.close();
+  console.log("データベース接続を閉じました。");
+}
