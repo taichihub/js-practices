184553ffc90e2d3e750d1e24e74231099392238d
taichihub <taichikuniv21@gmail.com>
2024-10-16 18:53:38 +0900

    ログの出力をprocessからconsoleに変更

--- 03.asynchronous/callback/existing_error.js
+++ 03.asynchronous/callback/existing_error.js
@@ -3,33 +3,31 @@ import { db } from "../db.js";
 db.run(
   "CREATE TABLE books (id INTEGER PRIMARY KEY AUTOINCREMENT, title TEXT NOT NULL UNIQUE)",
   () => {
-    process.stdout.write("テーブルが作成されました。\n");
+    console.log("テーブルが作成されました。");
 
     db.run(
       "INSERT INTO books (title) VALUES (?)",
       ["Node.js入門"],
       function () {
-        process.stdout.write(`行が追加されました。id: ${this.lastID}\n`);
+        console.log(`行が追加されました。id: ${this.lastID}`);
 
         db.run(
           "INSERT INTO books (title) VALUES (?)",
           ["Node.js入門"],
           function (err) {
             if (err) {
-              process.stderr.write(`エラーが発生しました: ${err.message}\n`);
+              console.error(`エラーが発生しました: ${err.message}`);
 
               db.all("SELECT * FROM books WHERE id = -1", [], (_, rows) => {
                 if (rows.length === 0) {
-                  process.stderr.write(
-                    "エラーが発生しました: レコードが存在しません\n",
-                  );
+                  console.error("エラーが発生しました: レコードが存在しません");
                 }
 
                 db.run("DROP TABLE books", () => {
-                  process.stdout.write("テーブルが削除されました。\n");
+                  console.log("テーブルが削除されました。");
 
                   db.close(() => {
-                    process.stdout.write("データベース接続を閉じました。\n");
+                    console.log("データベース接続を閉じました。");
                   });
                 });
               });
