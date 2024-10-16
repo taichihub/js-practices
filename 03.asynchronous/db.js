2fc8099fc98df4f64018f2b35c11ae9be33fb0b7
taichihub <taichikuniv21@gmail.com>
2024-10-16 17:14:50 +0900

    データベース接続の出力をprocessではなくconsole.logで対応

--- 03.asynchronous/db.js
+++ 03.asynchronous/db.js
@@ -1,7 +1,7 @@
 import sqlite3 from "sqlite3";
 
 export const db = new sqlite3.Database(":memory:", () => {
-  process.stdout.write("メモリ内のSQLiteデータベースに接続しました。\n");
+  console.log("メモリ内のSQLiteデータベースに接続しました。");
 });
 
 export function run(sql, params = []) {
