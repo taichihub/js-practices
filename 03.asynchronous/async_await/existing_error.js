e66bc94d5f7af6c14d9676a97dea8fb12ef76d2e
taichihub <taichikuniv21@gmail.com>
2024-10-21 18:20:52 +0900

    (async/await,エラーあり)ログ出力をprocessからconsoleに変更

--- 03.asynchronous/async_await/existing_error.js
+++ 03.asynchronous/async_await/existing_error.js
@@ -5,27 +5,27 @@ async function manageBooks() {
     await run(
       "CREATE TABLE books (id INTEGER PRIMARY KEY AUTOINCREMENT, title TEXT NOT NULL UNIQUE)",
     );
-    process.stdout.write("テーブルが作成されました。\n");
+    console.log("テーブルが作成されました。");
 
     const result1 = await run("INSERT INTO books (title) VALUES (?)", [
       "Node.js入門",
     ]);
-    process.stdout.write(`行が追加されました。id: ${result1.lastID}\n`);
+    console.log(`行が追加されました。id: ${result1.lastID}`);
 
     const result2 = await run("INSERT INTO books (title) VALUES (?)", [
       "Node.js入門",
     ]);
-    process.stdout.write(`行が追加されました。id: ${result2.lastID}\n`);
+    console.log(`行が追加されました。id: ${result2.lastID}`);
 
     const rows = await all("SELECT * FROM books");
     rows.forEach((row) => {
-      process.stdout.write(`${row.id}: ${row.title}\n`);
+      console.log(`${row.id}: ${row.title}`);
     });
 
     await run("DROP TABLE books");
-    process.stdout.write("テーブルが削除されました。\n");
+    console.log("テーブルが削除されました。");
   } catch (err) {
-    process.stderr.write(`エラーが発生しました: ${err.message}\n`);
+    console.error(`エラーが発生しました: ${err.message}`);
   } finally {
     close();
   }
