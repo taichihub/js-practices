132f71501a4236ebed8122ef3234184b7227dfe3
taichihub <taichikuniv21@gmail.com>
2024-10-21 17:56:59 +0900

    各SQL文修正

--- 03.asynchronous/promise/nonexistent_error.js
+++ 03.asynchronous/promise/nonexistent_error.js
@@ -1,21 +1,21 @@
 import { run, all, close } from "../db.js";
 
 run(
-  `CREATE TABLE IF NOT EXISTS books (id INTEGER PRIMARY KEY AUTOINCREMENT, title TEXT NOT NULL UNIQUE)`,
+  "CREATE TABLE books (id INTEGER PRIMARY KEY AUTOINCREMENT, title TEXT NOT NULL UNIQUE)",
 )
   .then(() => {
     console.log("テーブルが作成されました。");
-    return run(`INSERT INTO books (title) VALUES (?)`, ["Node.js入門"]);
+    return run("INSERT INTO books (title) VALUES (?)", ["Node.js入門"]);
   })
   .then((result) => {
     console.log(`行が追加されました。id: ${result.lastID}`);
-    return all(`SELECT id, title FROM books`);
+    return all("SELECT * FROM books");
   })
   .then((rows) => {
     rows.forEach((row) => {
       console.log(`${row.id}: ${row.title}`);
     });
-    return run(`DROP TABLE books`);
+    return run("DROP TABLE books");
   })
   .then(() => {
     console.log("テーブルが削除されました。");
