f576fe0be77f7b513a2e5f6f7e6dd619c48614ec
taichihub <taichikuniv21@gmail.com>
2024-10-21 18:18:54 +0900

    (async/await,エラーあり)各SQL文修正

--- 03.asynchronous/async_await/existing_error.js
+++ 03.asynchronous/async_await/existing_error.js
@@ -3,26 +3,26 @@ import { run, all, close } from "../db.js";
 async function manageBooks() {
   try {
     await run(
-      `CREATE TABLE IF NOT EXISTS books (id INTEGER PRIMARY KEY AUTOINCREMENT, title TEXT NOT NULL UNIQUE)`,
+      "CREATE TABLE books (id INTEGER PRIMARY KEY AUTOINCREMENT, title TEXT NOT NULL UNIQUE)",
     );
     process.stdout.write("テーブルが作成されました。\n");
 
-    const result1 = await run(`INSERT INTO books (title) VALUES (?)`, [
+    const result1 = await run("INSERT INTO books (title) VALUES (?)", [
       "Node.js入門",
     ]);
     process.stdout.write(`行が追加されました。id: ${result1.lastID}\n`);
 
-    const result2 = await run(`INSERT INTO books (title) VALUES (?)`, [
+    const result2 = await run("INSERT INTO books (title) VALUES (?)", [
       "Node.js入門",
     ]);
     process.stdout.write(`行が追加されました。id: ${result2.lastID}\n`);
 
-    const rows = await all(`SELECT id, title FROM books`);
+    const rows = await all("SELECT * FROM books");
     rows.forEach((row) => {
       process.stdout.write(`${row.id}: ${row.title}\n`);
     });
 
-    await run(`DROP TABLE books`);
+    await run("DROP TABLE books");
     process.stdout.write("テーブルが削除されました。\n");
   } catch (err) {
     process.stderr.write(`エラーが発生しました: ${err.message}\n`);
