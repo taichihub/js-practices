90a15e8b315531c4b7e693b442ce595e75bd53ab
taichihub <taichikuniv21@gmail.com>
2024-10-21 17:49:35 +0900

    不必要なバッククオートをダブルクオートに修正/SQL文のテーブル作成条件を修正

--- 03.asynchronous/promise/existing_error.js
+++ 03.asynchronous/promise/existing_error.js
@@ -1,28 +1,28 @@
 import { run, close } from "../db.js";
 
 run(
-  `CREATE TABLE IF NOT EXISTS books (id INTEGER PRIMARY KEY AUTOINCREMENT, title TEXT NOT NULL UNIQUE)`,
+  "CREATE TABLE books (id INTEGER PRIMARY KEY AUTOINCREMENT, title TEXT NOT NULL UNIQUE)",
 )
   .then(() => {
     console.log("テーブルが作成されました。");
-    return run(`INSERT INTO books (title) VALUES (?)`, ["Node.js入門"]);
+    return run("INSERT INTO books (title) VALUES (?)", ["Node.js入門"]);
   })
   .then((result) => {
     console.log(`行が追加されました。id: ${result.lastID}`);
-    return run(`INSERT INTO books (title) VALUES (?)`, ["Node.js入門"]);
+    return run("INSERT INTO books (title) VALUES (?)", ["Node.js入門"]);
   })
   .catch((err) => {
     console.error(`エラーが発生しました: ${err.message}`);
-    return run(`SELECT * FROM books WHERE id = ?`, [-1])
+    return run("SELECT * FROM books WHERE id = ?", [-1])
       .catch(() => {
         console.error("エラーが発生しました: レコードが存在しません");
       })
       .then(() => {
-        return run(`SELECT * FROM books`);
+        return run("SELECT * FROM books");
       })
       .then(() => {
         console.error("エラーが発生しました: レコードが存在しません");
-        return run(`DROP TABLE books`);
+        return run("DROP TABLE books");
       })
       .then(() => {
         console.log("テーブルが削除されました。");
