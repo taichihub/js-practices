1ed18fc954ed831c629df50f908f25fdfc6bc7fb
taichihub <taichikuniv21@gmail.com>
2024-10-21 17:45:11 +0900

    (Promise/エラーあり)ドキュメントの要件1〜4を満たすようプログラムを修正

--- 03.asynchronous/promise/existing_error.js
+++ 03.asynchronous/promise/existing_error.js
@@ -1,4 +1,4 @@
-import { run, all, close } from "../db.js";
+import { run, close } from "../db.js";
 
 run(
   `CREATE TABLE IF NOT EXISTS books (id INTEGER PRIMARY KEY AUTOINCREMENT, title TEXT NOT NULL UNIQUE)`,
@@ -11,21 +11,22 @@ run(
     process.stdout.write(`行が追加されました。id: ${result.lastID}\n`);
     return run(`INSERT INTO books (title) VALUES (?)`, ["Node.js入門"]);
   })
-  .then((result) => {
-    process.stdout.write(`もう一行が追加されました。id: ${result.lastID}\n`);
-    return all(`SELECT id, title FROM books`);
-  })
-  .then((rows) => {
-    rows.forEach((row) => {
-      process.stdout.write(`${row.id}: ${row.title}\n`);
-    });
-    return run(`DROP TABLE books`);
-  })
-  .then(() => {
-    process.stdout.write("テーブルが削除されました。\n");
-  })
   .catch((err) => {
     process.stderr.write(`エラーが発生しました: ${err.message}\n`);
+    return run(`SELECT * FROM books WHERE id = ?`, [-1])
+      .catch(() => {
+        process.stderr.write("エラーが発生しました: レコードが存在しません\n");
+      })
+      .then(() => {
+        return run(`SELECT * FROM books`);
+      })
+      .then(() => {
+        process.stderr.write("エラーが発生しました: レコードが存在しません\n");
+        return run(`DROP TABLE books`);
+      })
+      .then(() => {
+        process.stdout.write("テーブルが削除されました。\n");
+      });
   })
   .finally(() => {
     close();
