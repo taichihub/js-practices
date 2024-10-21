415cfcbc17e5e6365873d4e4f98f341ce5ab19d6
taichihub <taichikuniv21@gmail.com>
2024-10-21 17:47:25 +0900

    ログ出力をprocessからconsoleに修正

--- 03.asynchronous/promise/existing_error.js
+++ 03.asynchronous/promise/existing_error.js
@@ -4,28 +4,28 @@ run(
   `CREATE TABLE IF NOT EXISTS books (id INTEGER PRIMARY KEY AUTOINCREMENT, title TEXT NOT NULL UNIQUE)`,
 )
   .then(() => {
-    process.stdout.write("テーブルが作成されました。\n");
+    console.log("テーブルが作成されました。");
     return run(`INSERT INTO books (title) VALUES (?)`, ["Node.js入門"]);
   })
   .then((result) => {
-    process.stdout.write(`行が追加されました。id: ${result.lastID}\n`);
+    console.log(`行が追加されました。id: ${result.lastID}`);
     return run(`INSERT INTO books (title) VALUES (?)`, ["Node.js入門"]);
   })
   .catch((err) => {
-    process.stderr.write(`エラーが発生しました: ${err.message}\n`);
+    console.error(`エラーが発生しました: ${err.message}`);
     return run(`SELECT * FROM books WHERE id = ?`, [-1])
       .catch(() => {
-        process.stderr.write("エラーが発生しました: レコードが存在しません\n");
+        console.error("エラーが発生しました: レコードが存在しません");
       })
       .then(() => {
         return run(`SELECT * FROM books`);
       })
       .then(() => {
-        process.stderr.write("エラーが発生しました: レコードが存在しません\n");
+        console.error("エラーが発生しました: レコードが存在しません");
         return run(`DROP TABLE books`);
       })
       .then(() => {
-        process.stdout.write("テーブルが削除されました。\n");
+        console.log("テーブルが削除されました。");
       });
   })
   .finally(() => {
