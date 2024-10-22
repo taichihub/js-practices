638b579377381d0ad03b92f50374c5456748edc5
taichihub <taichikuniv21@gmail.com>
2024-10-22 13:22:15 +0900

    (async/await,エラーなし)Top-level awaitを使用して修正

--- 03.asynchronous/async_await/nonexistent_error.js
+++ 03.asynchronous/async_await/nonexistent_error.js
@@ -1,27 +1,23 @@
 import { run, all, close } from "../db.js";
 
-async function manageBooks() {
-  try {
-    await run(
-      "CREATE TABLE books (id INTEGER PRIMARY KEY AUTOINCREMENT, title TEXT NOT NULL UNIQUE)",
-    );
-    console.log("テーブルが作成されました。");
+try {
+  await run(
+    "CREATE TABLE books (id INTEGER PRIMARY KEY AUTOINCREMENT, title TEXT NOT NULL UNIQUE)",
+  );
+  console.log("テーブルが作成されました。");
 
-    const result = await run("INSERT INTO books (title) VALUES (?)", [
-      "Node.js入門",
-    ]);
-    console.log(`行が追加されました。id: ${result.lastID}`);
+  const result = await run("INSERT INTO books (title) VALUES (?)", [
+    "Node.js入門",
+  ]);
+  console.log(`行が追加されました。id: ${result.lastID}`);
 
-    const rows = await all("SELECT * FROM books");
-    rows.forEach((row) => {
-      console.log(`${row.id}: ${row.title}`);
-    });
+  const rows = await all("SELECT * FROM books");
+  rows.forEach((row) => {
+    console.log(`${row.id}: ${row.title}`);
+  });
 
-    await run("DROP TABLE books");
-    console.log("テーブルが削除されました。");
-  } finally {
-    close();
-  }
+  await run("DROP TABLE books");
+  console.log("テーブルが削除されました。");
+} finally {
+  close();
 }
-
-manageBooks();
