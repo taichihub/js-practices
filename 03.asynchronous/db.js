027c90fb25317ab50cf1eb7d22e6bf0f44532254
taichihub <taichikuniv21@gmail.com>
2024-10-22 17:33:03 +0900

    Databaseクラスを作成し、再利用性を高めた

--- 03.asynchronous/db.js
+++ 03.asynchronous/db.js
@@ -1,34 +1,47 @@
 import sqlite3 from "sqlite3";
 
-export const db = new sqlite3.Database(":memory:", () => {
-  console.log("メモリ内のSQLiteデータベースに接続しました。");
-});
+export class Database {
+  constructor(driver) {
+    this.db = driver;
+  }
 
-export function run(sql, params = []) {
-  return new Promise((resolve, reject) => {
-    db.run(sql, params, function (err) {
-      if (err) {
-        reject(err);
-      } else {
-        resolve({ lastID: this.lastID, changes: this.changes });
-      }
+  run(sql, params = []) {
+    return new Promise((resolve, reject) => {
+      this.db.run(sql, params, function (err) {
+        if (err) {
+          reject(err);
+        } else {
+          resolve({ lastID: this.lastID, changes: this.changes });
+        }
+      });
     });
-  });
+  }
+
+  all(sql, params = []) {
+    return new Promise((resolve, reject) => {
+      this.db.all(sql, params, (err, rows) => {
+        if (err) {
+          reject(err);
+        } else {
+          resolve(rows);
+        }
+      });
+    });
+  }
+
+  close() {
+    this.db.close();
+    process.stdout.write("データベース接続を閉じました。\n");
+  }
 }
 
-export function all(sql, params = []) {
-  return new Promise((resolve, reject) => {
-    db.all(sql, params, (err, rows) => {
-      if (err) {
-        reject(err);
-      } else {
-        resolve(rows);
-      }
-    });
+export function createSQLiteDatabase() {
+  const db = new sqlite3.Database(":memory:", () => {
+    console.log("メモリ内のSQLiteデータベースに接続しました。");
   });
+  return new Database(db);
 }
 
-export function close() {
-  db.close();
-  process.stdout.write("データベース接続を閉じました。\n");
+export function loadDatabase() {
+  return createSQLiteDatabase();
 }
