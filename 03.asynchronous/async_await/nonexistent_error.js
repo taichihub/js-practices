6c496ca14ac99d5b6efd9387e6ca2ebdb9782b86
taichihub <taichikuniv21@gmail.com>
2024-10-22 09:57:42 +0900

    (async/await,エラーなし)SQL文修正

--- 03.asynchronous/async_await/nonexistent_error.js
+++ 03.asynchronous/async_await/nonexistent_error.js
@@ -3,21 +3,21 @@ import { run, all, close } from "../db.js";
 async function manageBooks() {
   try {
     await run(
-      `CREATE TABLE IF NOT EXISTS books (id INTEGER PRIMARY KEY AUTOINCREMENT, title TEXT NOT NULL UNIQUE)`,
+      "CREATE TABLE books (id INTEGER PRIMARY KEY AUTOINCREMENT, title TEXT NOT NULL UNIQUE)",
     );
     process.stdout.write("テーブルが作成されました。\n");
 
-    const result = await run(`INSERT INTO books (title) VALUES (?)`, [
+    const result = await run("INSERT INTO books (title) VALUES (?)", [
       "Node.js入門",
     ]);
     process.stdout.write(`行が追加されました。id: ${result.lastID}\n`);
 
-    const rows = await all(`SELECT id, title FROM books`);
+    const rows = await all("SELECT * FROM books");
     rows.forEach((row) => {
       process.stdout.write(`${row.id}: ${row.title}\n`);
     });
 
-    await run(`DROP TABLE books`);
+    await run("DROP TABLE books");
     process.stdout.write("テーブルが削除されました。\n");
   } finally {
     close();
