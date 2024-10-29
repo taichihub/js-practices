ecc1787f0b2d991f346b4f5bdfe22e05cfc96175
taichihub <taichikuniv21@gmail.com>
2024-10-29 19:19:38 +0900

    日数表示の際のpadStartの値2の変数を解消

--- 02.calendar/cal.js
+++ 02.calendar/cal.js
@@ -22,8 +22,7 @@ function printCalendar(year, month) {
   process.stdout.write(padding);
 
   for (let day = firstDay; day <= lastDay; day = day.plus({ days: 1 })) {
-    const padStartBetweenDay = 2;
-    const formattedDay = day.day.toString().padStart(padStartBetweenDay);
+    const formattedDay = day.day.toString().padStart(2);
 
     if (day.weekday === 6 || day.hasSame(lastDay, "day")) {
       console.log(formattedDay);
