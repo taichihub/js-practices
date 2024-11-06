016ef1bdc3a55f04963b75753fc678e0fdb87dd5
taichihub <taichikuniv21@gmail.com>
2024-11-06 17:15:33 +0900

    エラーハンドリング周りの処理を修正

--- 03.asynchronous/async_await/existing_error.js
+++ 03.asynchronous/async_await/existing_error.js
@@ -8,22 +8,17 @@ try {
 
   const titles = ["Node.js入門", "Node.js入門"];
   for (const title of titles) {
-    try {
-      const result = await run("INSERT INTO books (title) VALUES (?)", [title]);
-      console.log(`行が追加されました。id: ${result.lastID}`);
-    } catch (err) {
-      throw new Error(err.message);
-    }
+    const result = await run("INSERT INTO books (title) VALUES (?)", [title]);
+    console.log(`行が追加されました。id: ${result.lastID}`);
   }
 } catch (err) {
-  if (err instanceof Error) {
-    console.error(`エラーが発生しました: ${err.message}`);
-  }
-  try {
-    await run("SELECT * FROM nonexistent_table WHERE id = ?", [-1]);
-  } catch {
-    console.error(`エラーが発生しました: ${err.message}`);
-  }
+  console.error(`エラーが発生しました: ${err.message}`);
+}
+
+try {
+  await run("SELECT * FROM nonexistent_table WHERE id = ?", [-1]);
+} catch (err) {
+  console.error(`エラーが発生しました: ${err.message}`);
   await run("DROP TABLE books");
   console.log("テーブルが削除されました。");
 } finally {
