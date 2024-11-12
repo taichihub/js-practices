983d77b28a828a7d5a95583b513b38661f6f5821
taichihub <taichikuniv21@gmail.com>
2024-11-12 18:52:50 +0900

    dbが正常に作成された後に後続の処理が働くようにした

--- 03.asynchronous/callback/existing_error.js
+++ 03.asynchronous/callback/existing_error.js
@@ -1,40 +1,43 @@
-import { db } from "../db_operations.js";
+import sqlite3 from "sqlite3";
 
-db.run(
-  "CREATE TABLE books (id INTEGER PRIMARY KEY AUTOINCREMENT, title TEXT NOT NULL UNIQUE)",
-  () => {
-    console.log("テーブルが作成されました。");
+const db = new sqlite3.Database(":memory:", () => {
+  console.log("メモリ内のSQLiteデータベースに接続しました。");
+  db.run(
+    "CREATE TABLE books (id INTEGER PRIMARY KEY AUTOINCREMENT, title TEXT NOT NULL UNIQUE)",
+    () => {
+      console.log("テーブルが作成されました。");
 
-    db.run(
-      "INSERT INTO books (title) VALUES (?)",
-      ["Node.js入門"],
-      function () {
-        console.log(`行が追加されました。id: ${this.lastID}`);
+      db.run(
+        "INSERT INTO books (title) VALUES (?)",
+        ["Node.js入門"],
+        function () {
+          console.log(`行が追加されました。id: ${this.lastID}`);
 
-        db.run(
-          "INSERT INTO books (title) VALUES (?)",
-          ["Node.js入門"],
-          (err) => {
-            if (err) {
-              console.error(`エラーが発生しました: ${err.message}`);
-            }
-
-            db.all("SELECT content FROM books", (err) => {
+          db.run(
+            "INSERT INTO books (title) VALUES (?)",
+            ["Node.js入門"],
+            (err) => {
               if (err) {
                 console.error(`エラーが発生しました: ${err.message}`);
               }
 
-              db.run("DROP TABLE books", () => {
-                console.log("テーブルが削除されました。");
+              db.all("SELECT content FROM books", (err) => {
+                if (err) {
+                  console.error(`エラーが発生しました: ${err.message}`);
+                }
 
-                db.close(() => {
-                  console.log("データベース接続を閉じました。");
+                db.run("DROP TABLE books", () => {
+                  console.log("テーブルが削除されました。");
+
+                  db.close(() => {
+                    console.log("データベース接続を閉じました。");
+                  });
                 });
               });
-            });
-          },
-        );
-      },
-    );
-  },
-);
+            },
+          );
+        },
+      );
+    },
+  );
+});
