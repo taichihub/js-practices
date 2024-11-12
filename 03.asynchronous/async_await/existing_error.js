736ca2eb404ac6a0a2d0b88d86df9d4b70d696ae
taichihub <taichikuniv21@gmail.com>
2024-11-12 21:39:02 +0900

    finallyが実行される内容と範囲を調整

--- 03.asynchronous/async_await/existing_error.js
+++ 03.asynchronous/async_await/existing_error.js
@@ -9,27 +9,32 @@ try {
   );
   console.log("テーブルが作成されました。");
 
-  const titles = ["Node.js入門", "Node.js入門"];
-  for (const title of titles) {
-    const result = await run(db, "INSERT INTO books (title) VALUES (?)", [
-      title,
-    ]);
-    console.log(`行が追加されました。id: ${result.lastID}`);
+  try {
+    const titles = ["Node.js入門", "Node.js入門"];
+    for (const title of titles) {
+      const result = await run(db, "INSERT INTO books (title) VALUES (?)", [
+        title,
+      ]);
+      console.log(`行が追加されました。id: ${result.lastID}`);
+    }
+  } catch (err) {
+    console.error(`エラーが発生しました: ${err.message}`);
   }
-} catch (err) {
-  console.error(`エラーが発生しました: ${err.message}`);
-}
 
-try {
-  const result = await all(db, "SELECT * FROM nonexistent_table WHERE id = ?", [
-    -1,
-  ]);
-  console.log(result);
-} catch (err) {
-  console.error(`エラーが発生しました: ${err.message}`);
-} finally {
+  try {
+    const result = await all(
+      db,
+      "SELECT * FROM nonexistent_table WHERE id = ?",
+      [-1],
+    );
+    console.log(result);
+  } catch (err) {
+    console.error(`エラーが発生しました: ${err.message}`);
+  }
+
   await run(db, "DROP TABLE books");
   console.log("テーブルが削除されました。");
+} finally {
   await close(db);
   console.log("データベース接続を閉じました。");
 }
