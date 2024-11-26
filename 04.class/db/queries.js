874405bce02dac07385e035156ff1d6a926863e0
taichihub <taichikuniv21@gmail.com>
2024-11-26 15:31:27 +0900

    全メモ取得のクエリを最新順で取得するようにした

--- 04.class/db/queries.js
+++ 04.class/db/queries.js
@@ -1,6 +1,6 @@
 export const MEMOS_TABLE_CREATION =
   "CREATE TABLE IF NOT EXISTS memos (id INTEGER PRIMARY KEY AUTOINCREMENT, memo TEXT)";
 export const MEMO_INSERTION = "INSERT INTO memos (memo) VALUES (?)";
-export const ALL_MEMOS_SELECTION = "SELECT id, memo FROM memos";
+export const ALL_MEMOS_SELECTION = "SELECT * FROM memos ORDER BY id DESC";
 export const MEMO_SELECTION_BY_ID = "SELECT * FROM memos WHERE id = ?";
 export const MEMO_DELETION_BY_ID = "DELETE FROM memos WHERE id = ?";
