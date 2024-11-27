5ce7476c398d4c277c5834bd7173cb2b676dfdb4
taichihub <taichikuniv21@gmail.com>
2024-11-27 17:05:21 +0900

    ログファイルにログを追加

--- 04.class/config/log.js
+++ 04.class/config/log.js
@@ -21,22 +21,31 @@ export const ADD_MEMO_LOG_MESSAGES = {
     "メモの内容を入力してください（終了するにはCtrl + Dを押してください）:\n",
   EMPTY: "メモの内容が空です。保存されませんでした。\n",
   SUCCESS: "メモを追加しました。\n",
+  STDIN_ERROR: "標準入力でエラーが発生しました: ",
+  SAVE_ERROR: "メモの保存処理中にエラーが発生しました: ",
+  INPUT_ERROR: "入力中にエラーが発生しました: ",
+  INSERT_ERROR: "データベースへのインサート処理中にエラーが発生しました: ",
 };
 
 export const LIST_MEMOS_LOG_MESSAGES = {
   TITLE: "メモ一覧:\n",
   NOT_FOUND: "メモが存在しません。\n",
+  FETCH_ERROR: "メモの取得中にエラーが発生しました: ",
+  SHOW_ERROR: "メモの表示中にエラーが発生しました: ",
 };
 
 export const READ_MEMO_LOG_MESSAGES = {
   PROMPT: "表示するメモを選んでください:",
   NOT_FOUND: "メモが見つかりません。\n",
+  FETCH_ERROR: "メモの取得中にエラーが発生しました: ",
 };
 
 export const DELETE_MEMO_LOG_MESSAGES = {
   PROMPT: "削除するメモを選んでください:",
   SUCCESS: "メモを削除しました。\n",
   NOT_FOUND: "削除するメモが見つかりません。\n",
+  FETCH_ERROR: "メモの取得中にエラーが発生しました: ",
+  DELETE_ERROR: "メモの削除中にエラーが発生しました: ",
 };
 
 export const MEMO_HELPERS_LOG_MESSAGES = {
