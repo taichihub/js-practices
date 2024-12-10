5a9d3bf744701c22d5f8dd6a0a9ae4ea9178d228
taichihub <taichikuniv21@gmail.com>
2024-12-10 22:30:13 +0900

    addMemoの処理をPromiseベースで修正した

--- 04.class/actions/addMemo.js
+++ 04.class/actions/addMemo.js
@@ -4,22 +4,24 @@ import { ADD_MEMO_LOG_MESSAGES } from "../config/log.js";
 import { checkIfEmpty } from "../helpers/memoHelpers.js";
 
 export async function addMemo(database) {
-  const rl = createInterface({ input, output });
-
-  if (input.isTTY) {
-    process.stdout.write(`${ADD_MEMO_LOG_MESSAGES.PROMPT}`);
+  try {
+    const content = await getInputLines(input, output);
+    checkIfEmpty(content, ADD_MEMO_LOG_MESSAGES.EMPTY);
+    const memoContent = content.join("\n");
+    await database.insertMemo(memoContent.trim());
+    process.stdout.write(`${ADD_MEMO_LOG_MESSAGES.SUCCESS}`);
+  } catch (err) {
+    process.stderr.write(`${ADD_MEMO_LOG_MESSAGES.SAVE_ERROR}${err.message}`);
   }
+}
 
-  const content = [];
-  rl.on("line", (line) => content.push(line));
-  rl.on("close", async () => {
-    try {
-      checkIfEmpty(content, ADD_MEMO_LOG_MESSAGES.EMPTY);
-      const memoContent = content.join("\n");
-      await database.insertMemo(memoContent.trim());
-      process.stdout.write(`${ADD_MEMO_LOG_MESSAGES.SUCCESS}`);
-    } catch (err) {
-      process.stderr.write(`${ADD_MEMO_LOG_MESSAGES.SAVE_ERROR}${err.message}`);
-    }
+function getInputLines(input, output) {
+  return new Promise((resolve, reject) => {
+    const rl = createInterface({ input, output });
+    const content = [];
+
+    rl.on("line", (line) => content.push(line));
+    rl.on("close", () => resolve(content));
+    rl.on("error", (err) => reject(err));
   });
 }
