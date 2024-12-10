5389332e60e6f3e9cd539a62b37d6182154fdaea
taichihub <taichikuniv21@gmail.com>
2024-12-10 22:16:34 +0900

    TTYと非TTYのメモ追加ロジックを統一させた

--- 04.class/actions/addMemo.js
+++ 04.class/actions/addMemo.js
@@ -2,33 +2,13 @@ import { createInterface } from "readline";
 import { stdin as input, stdout as output } from "process";
 import { ADD_MEMO_LOG_MESSAGES } from "../config/log.js";
 import { checkIfEmpty } from "../helpers/memoHelpers.js";
-import { FILE_ENCODING } from "../config/settings.js";
 
 export async function addMemo(database) {
-  if (!input.isTTY) {
-    await nonInteractiveInput(database);
-  } else {
-    await interactiveInput(database);
-  }
-}
-
-async function nonInteractiveInput(database) {
-  try {
-    const chunks = [];
-    for await (const chunk of input) {
-      chunks.push(chunk);
-    }
-    const data = Buffer.concat(chunks).toString(FILE_ENCODING);
-    await database.insertMemo(data.trim());
-    process.stdout.write(`${ADD_MEMO_LOG_MESSAGES.SUCCESS}`);
-  } catch (err) {
-    process.stderr.write(`${ADD_MEMO_LOG_MESSAGES.STDIN_ERROR}${err.message}`);
-  }
-}
-
-async function interactiveInput(database) {
   const rl = createInterface({ input, output });
-  process.stdout.write(`${ADD_MEMO_LOG_MESSAGES.PROMPT}`);
+
+  if (input.isTTY) {
+    process.stdout.write(`${ADD_MEMO_LOG_MESSAGES.PROMPT}`);
+  }
 
   const content = [];
   rl.on("line", (line) => content.push(line));
