24d614adebbbbb1806c3d6021b9c36d8071022ed
taichihub <taichikuniv21@gmail.com>
2024-12-18 20:22:17 +0900

    メモがない場合のreturn処理を追加

--- 04.class/actions/deleteMemo.js
+++ 04.class/actions/deleteMemo.js
@@ -2,6 +2,7 @@ import { selectMemo } from "../helpers/memoHelpers.js";
 
 export async function deleteMemo(database) {
   const memo = await selectMemo(database, "削除するメモを選んでください:");
+  if (!memo) return;
   await database.deleteMemoById(memo.id);
   console.log("メモを削除しました。");
 }
--- 04.class/actions/readMemo.js
+++ 04.class/actions/readMemo.js
@@ -2,5 +2,6 @@ import { selectMemo } from "../helpers/memoHelpers.js";
 
 export async function readMemo(database) {
   const memo = await selectMemo(database, "表示するメモを選んでください:");
+  if (!memo) return;
   console.log(memo.content);
 }
